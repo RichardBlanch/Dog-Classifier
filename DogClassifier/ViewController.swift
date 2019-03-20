84d3eace17d5ee508444993a8c8355833c606508
RichardBlanch <rblanchard@grio.com>
2019-03-20 16:16:43 -0700

    Initial

---
+++ DogClassifier/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DogClassifier
+//
+//  Created by Richard Blanchard on 3/20/19.
+//  Copyright © 2019 Richard Blanchard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
