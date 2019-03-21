81641a4cbe1e398eaf854ebd4f3cfc8ef989c994
RichardBlanch <rblanchard@grio.com>
2019-03-21 10:07:03 -0700

    Add git ignore

---
+++ DogClassifier/Extension/UIImage+Extension.swift
@@ -0,0 +1,42 @@
+//
+//  UIImage+Extension.swift
+//  DogClassifier
+//
+//  Created by Richard Blanchard on 3/21/19.
+//  Copyright © 2019 Richard Blanchard. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+extension UIImage {
+    var cvPixelBuffer: CVPixelBuffer? {
+        let height: CGFloat = size.height
+        let width: CGFloat = size.width
+
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        var pixelBuffer : CVPixelBuffer?
+
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(width), Int(height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
+
+        guard (status == kCVReturnSuccess) else {
+            return nil
+        }
+        
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
+        
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        let context = CGContext(data: pixelData, width: Int(width), height: Int(height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
+        
+        context?.translateBy(x: 0, y: size.height)
+        context?.scaleBy(x: 1.0, y: -1.0)
+        
+        UIGraphicsPushContext(context!)
+        draw(in: CGRect(x: 0, y: 0, width: width, height: height))
+        UIGraphicsPopContext()
+        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        
+        return pixelBuffer
+    }
+}
--- DogClassifier/ViewController.swift
+++ DogClassifier/ViewController.swift
@@ -10,11 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet private weak var containerStackView: UIStackView!
+    @IBOutlet private weak var dogImageView: UIImageView!
+    @IBOutlet private weak var dogImageViewAspectRatio: NSLayoutConstraint!
+    @IBOutlet private weak var dogLabel: UILabel!
+    @IBOutlet private weak var submitButton: UIButton!
+    
+    @IBAction func didTapSubmit(_ sender: UIButton) {
+        let imagePickerViewController = UIImagePickerController()
+        imagePickerViewController.delegate = self
+        
+        present(imagePickerViewController, animated: true, completion: nil)
     }
-
-
 }
 
+extension ViewController: UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+
+        if let dogImage = info[.originalImage] as? UIImage, let pixelBuffer = dogImage.cvPixelBuffer {
+            do {
+                dogImageView.image = dogImage
+                dogImageViewAspectRatio.constant = dogImage.size.width / dogImage.size.height
+
+                UIView.animate(withDuration: 1.0) {
+                    self.view.layoutIfNeeded()
+                }
+        
+                
+            } catch {
+                print("Error: \(error)")
+            }
+        }
+    }
+    
+    
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+}
